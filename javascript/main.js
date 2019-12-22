fd3853c07bc3774ad262b1c0531b5aa5b73cfbdc
Jared Allen <jaredallen2310@gmail.com>
2019-12-22 00:20:31 -0800

    Format corrections
    added main.js and javascript folder, added additional text for messages

---
+++ javascript/main.js
@@ -0,0 +1,66 @@
+	
+	var person = prompt("Please input your first name...");
+ 	
+	document.getElementById("greeting").innerHTML = "Merry Christmas " + person + "!";
+
+	var wedding = '<iframe src= "https://player.vimeo.com/video/379119655" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>'
+
+ 	if (person == 'Dana') {document.getElementById("video").innerHTML = "Dana, I'm so glad my dad met you, you both are so happy together and I'm glad I could be there to see it. Love you all!" + "<br>" + wedding}
+ 	else if (person == 'dana') {document.getElementById("video").innerHTML = "Dana, I'm so glad my dad met you, you both are so happy together and I'm glad I could be there to see it. Love you all!" + "<br>" + wedding}
+ 	
+ 	else if (person == 'Dave') {document.getElementById("video").innerHTML = "Dad, you've always been there for me even when I least expect it. I apreciate and cherish every moment we spent together, love you Dad!" + "<br>" + wedding}
+ 	else if (person == 'dave') {document.getElementById("video").innerHTML = "Dad, you've always been there for me even when I least expect it. I apreciate and cherish every moment we spent together, love you Dad!" + "<br>" + wedding}
+ 	else if (person == 'Dad') {document.getElementById("video").innerHTML = "Dad, you've always been there for me even when I least expect it. I apreciate and cherish every moment we spent together, love you Dad!" + "<br>" + wedding}
+ 	else if (person == 'dad') {document.getElementById("video").innerHTML = "Dad, you've always been there for me even when I least expect it. I apreciate and cherish every moment we spent together, love you Dad!" + "<br>" + wedding}
+
+ 	else if (person == 'Haelee') {document.getElementById("video").innerHTML ="Haelee, I hope y'all have a perfect Christmas, love you all very much!"}
+ 	else if (person == 'haelee') {document.getElementById("video").innerHTML ="Haelee, I hope y'all have a perfect Christmas, love you all very much!"}
+	else if (person == 'Delanee') {document.getElementById("video").innerHTML ="Delanee, you all have been so inspirational to me personally and I can't thank you enough. Love you all!"}
+	else if (person == 'delanee') {document.getElementById("video").innerHTML ="Delanee, you all have been so inspirational to me personally and I can't thank you enough. Love you all!"}
+
+
+	else if (person == 'Jade') {document.getElementById("video").innerHTML ="Jade, I can't thank you enough for the motivation and inspiration you give me on a regular basis. I would say that you are the best, but you already know it. Love you Jade!"}
+	else if (person == 'jade') {document.getElementById("video").innerHTML ="Jade, I can't thank you enough for the motivation and inspiration you give me on a regular basis. I would say that you are the best, but you already know it. Love you Jade!"}
+
+	else if (person == 'Christina') {document.getElementById("video").innerHTML ="Christina, you are pretty rad whether you know it or not. You best not forget about them boys at the zoo. Love ya!"}
+	else if (person == 'christina') {document.getElementById("video").innerHTML ="Christina, you are pretty rad whether you know it or not. You best not forget about them boys at the zoo. Love ya!"}
+
+	else if (person == 'Donna') {document.getElementById("video").innerHTML ="hello"}
+	else if (person == 'donna') {document.getElementById("video").innerHTML ="hello"}
+
+	else if (person == 'Dakota') {document.getElementById("video").innerHTML ="hello"}
+	else if (person == 'dakota') {document.getElementById("video").innerHTML ="hello"}
+
+	else if (person == 'Ashliegh') {document.getElementById("video").innerHTML ="hello"}
+	else if (person == 'ashliegh') {document.getElementById("video").innerHTML ="hello"}
+	else if (person == 'Ash') {document.getElementById("video").innerHTML ="hello"}
+	else if (person == 'ash') {document.getElementById("video").innerHTML ="hello"}
+
+	else if (person == 'Jagger') {document.getElementById("video").innerHTML ="hello"}
+	else if (person == 'jagger') {document.getElementById("video").innerHTML ="hello"}
+
+ 	else if (person == 'Mom') {document.getElementById("video").innerHTML ="Mom, there really aint much I gotta say now huh? Ya already know I love ya, and that aint gonna change. But you have been a huge motivation to me ever since I almost drove that bus through the school, and when I tell people I'm a 'momma's boy' I ain't lying. Love you mom!"}
+ 	else if (person == 'mom') {document.getElementById("video").innerHTML = "Mom, there really aint much I gotta say now huh? Ya already know I love ya, and that aint gonna change. But you have been a huge motivation to me ever since I almost drove that bus through the school, and when I tell people I'm a 'momma's boy' I ain't lying. Love you mom!"}
+ 	else if (person == 'Tamie') {document.getElementById("video").innerHTML = "Mom, there really aint much I gotta say now huh? Ya already know I love ya, and that aint gonna change. But you have been a huge motivation to me ever since I almost drove that bus through the school, and when I tell people I'm a 'momma's boy' I ain't lying. Love you mom!"}
+ 	else if (person == 'tamie') {document.getElementById("video").innerHTML = "Mom, there really aint much I gotta say now huh? Ya already know I love ya, and that aint gonna change. But you have been a huge motivation to me ever since I almost drove that bus through the school, and when I tell people I'm a 'momma's boy' I ain't lying. Love you mom!"}
+
+ 		
+	else if (person == 'Brian') {document.getElementById("video").innerHTML = ""}
+	else if (person == 'brian') {document.getElementById("video").innerHTML = ""}
+	else if (person == 'Bran') {document.getElementById("video").innerHTML = ""}
+	else if (person == 'bran') {document.getElementById("video").innerHTML = ""}
+
+	else if (person == 'Scruffy') {document.getElementById("video").innerHTML = "Scruffy, you are the same goofy big boy we all need and deserve. Love ya man."}
+	else if (person == 'scruffy') {document.getElementById("video").innerHTML = "Scruffy, you are the same goofy big boy we all need and deserve. Love ya man."}
+
+	else if (person == 'Garrett') {document.getElementById("video").innerHTML = ""}
+	else if (person == 'garrett') {document.getElementById("video").innerHTML = ""}
+
+
+	else if (person == 'Yordle') {document.getElementById("video").innerHTML = "Yordle, when you aren't seducing doors you are finding yet another way to throw us all off base, and I love it. Love ya man."}
+	else if (person == 'yordle') {document.getElementById("video").innerHTML = "Yordle, when you aren't seducing doors you are finding yet another way to throw us all off base, and I love it. Love ya man."}
+
+
+	else {document.getElementById("video").innerHTML = "Hope your holidays are amazing!" };
+
+	
